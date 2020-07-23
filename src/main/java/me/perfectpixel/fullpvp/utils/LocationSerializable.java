39bf4b4ef23bccedf9e14030e3c2883d3f578296
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-23 18:31:42 -0500

    Just change methods names!

--- src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
+++ src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
@@ -7,13 +7,13 @@ public interface LocationSerializable {
 
     Location getLocation();
 
-    default Location deserialize(String location) {
+    default Location fromString(String location) {
         String[] split = location.split(";");
 
         return new Location(Bukkit.getWorld(split[0]), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
     }
 
-    default String serialize() {
+    default String fromBukkit() {
         Location location = getLocation();
 
         return location.getWorld().getName() + ";" + location.getX() + ";" + location.getY() + ";" + location.getZ();
