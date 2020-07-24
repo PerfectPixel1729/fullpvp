7c4a57c50603571c50ec780f212544103d725a22
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-24 08:45:54 -0500

    Just finishing DefaultSupplierChest, now are running!

--- src/main/java/me/perfectpixel/fullpvp/chest/DefaultSupplierChest.java
+++ src/main/java/me/perfectpixel/fullpvp/chest/DefaultSupplierChest.java
@@ -2,15 +2,29 @@ package me.perfectpixel.fullpvp.chest;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 import org.bukkit.Location;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 public class DefaultSupplierChest implements SupplierChest {
 
     private final String name;
-    private final Inventory inventory;
+    private final Map<Integer, ItemStack> items;
     private final Location location;
+
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> supplierChestMap = new HashMap<>();
+
+        supplierChestMap.put("location", fromBukkit());
+        items.forEach((slot, item) -> supplierChestMap.put("items." + slot, item.serialize()));
+
+        return supplierChestMap;
+    }
 
 }
