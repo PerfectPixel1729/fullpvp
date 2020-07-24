8fa150f174511b1e7dc915d34f5be9c1b0a2219b
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-24 08:46:40 -0500

    Just migrating one method to static from more utilities.

--- src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
+++ src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
@@ -7,7 +7,7 @@ public interface LocationSerializable {
 
     Location getLocation();
 
-    default Location fromString(String location) {
+    static Location fromString(String location) {
         String[] split = location.split(";");
 
         return new Location(Bukkit.getWorld(split[0]), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
