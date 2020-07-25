3f81bcab2325b7c9d6b1a7b9e37048843ad7c275
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-24 23:15:39 -0500

    Just change some variable name!

--- src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
+++ src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
@@ -4,6 +4,7 @@ import me.perfectpixel.fullpvp.Storage;
 import me.perfectpixel.fullpvp.chest.SupplierChest;
 import me.perfectpixel.fullpvp.chest.SupplierChestStorageManager;
 import me.perfectpixel.fullpvp.chest.creator.SupplierChestCreatorCache;
+import me.perfectpixel.fullpvp.chest.creator.UserCreator;
 import me.perfectpixel.fullpvp.user.User;
 import me.perfectpixel.fullpvp.user.UserStorageManager;
 
@@ -12,8 +13,6 @@ import me.yushust.inject.identity.Key;
 import me.yushust.inject.identity.type.TypeReference;
 import me.yushust.inject.name.Names;
 
-import org.bukkit.Location;
-
 import java.util.UUID;
 
 public class StorageModule extends AbstractModule {
@@ -21,7 +20,7 @@ public class StorageModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Key.of(new TypeReference<Storage<User, UUID>>() {}, (Names.named("users")))).to(UserStorageManager.class).singleton();
-        bind(Key.of(new TypeReference<Storage<Location, UUID>>() {}, (Names.named("chests-creators")))).to(SupplierChestCreatorCache.class).singleton();
+        bind(Key.of(new TypeReference<Storage<UserCreator, UUID>>() {}, (Names.named("chests-creators")))).to(SupplierChestCreatorCache.class).singleton();
         bind(Key.of(new TypeReference<Storage<SupplierChest, String>>() {}, (Names.named("chests")))).to(SupplierChestStorageManager.class).singleton();
     }
 
