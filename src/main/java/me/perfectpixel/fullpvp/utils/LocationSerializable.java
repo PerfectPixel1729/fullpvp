cc108c32486a7477cec5f87caf2f0cb14c8ec7b7
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-25 16:33:05 -0500

    All methods statics, to be more functional.

--- src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
+++ src/main/java/me/perfectpixel/fullpvp/utils/LocationSerializable.java
@@ -5,17 +5,13 @@ import org.bukkit.Location;
 
 public interface LocationSerializable {
 
-    Location getLocation();
-
     static Location fromString(String location) {
         String[] split = location.split(";");
 
         return new Location(Bukkit.getWorld(split[0]), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
     }
 
-    default String fromBukkit() {
-        Location location = getLocation();
-
+    static String fromBukkit(Location location) {
         return location.getWorld().getName() + ";" + location.getX() + ";" + location.getY() + ";" + location.getZ();
     }
 
