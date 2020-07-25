1f98fe853586cf201e0be55df396e97e5b23956a
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-25 16:30:35 -0500

    Remove key name, now key location.

--- src/main/java/me/perfectpixel/fullpvp/chest/SupplierChestStorageManager.java
+++ src/main/java/me/perfectpixel/fullpvp/chest/SupplierChestStorageManager.java
@@ -2,62 +2,60 @@ package me.perfectpixel.fullpvp.chest;
 
 import me.perfectpixel.fullpvp.Storage;
 import me.perfectpixel.fullpvp.files.FileManager;
-
 import me.perfectpixel.fullpvp.utils.LocationSerializable;
+
 import me.yushust.inject.Inject;
 import me.yushust.inject.name.Named;
-import org.bukkit.inventory.ItemStack;
+
+import org.bukkit.Location;
 
 import java.util.*;
 
-public class SupplierChestStorageManager implements Storage<SupplierChest, String> {
+public class SupplierChestStorageManager implements Storage<SupplierChest, Location> {
 
     @Inject
     @Named("chests")
     private FileManager chests;
 
-    private final Map<String, SupplierChest> supplierChests = new HashMap<>();
-
-
+    private static final Map<Location, SupplierChest> SUPPLIER_CHESTS = new HashMap<>();
+    
     @Override
-    public Map<String, SupplierChest> get() {
-        return supplierChests;
+    public Map<Location, SupplierChest> get() {
+        return SUPPLIER_CHESTS;
     }
 
     @Override
-    public Optional<SupplierChest> find(String s) {
-        return Optional.ofNullable(supplierChests.get(s));
+    public Optional<SupplierChest> find(Location location) {
+        return Optional.ofNullable(SUPPLIER_CHESTS.get(location));
     }
 
     @Override
-    public Optional<SupplierChest> findFromData(String s) {
-        if (!chests.contains("chests." + s)) {
-            return Optional.empty();
-        }
+    public Optional<SupplierChest> findFromData(Location location) { return Optional.empty(); }
 
-        return Optional.of(new DefaultSupplierChest((Map<Integer, ItemStack>) chests.get("chests." + s + ".items"), LocationSerializable.fromString(chests.getString("chests." + s + ".location"))));
+    public static boolean containsChest(String name) {
+        return SUPPLIER_CHESTS.values().stream().anyMatch(supplierChest -> supplierChest.getName().equals(name));
     }
 
     @Override
-    public void save(String s) {
-        find(s).ifPresent(supplierChest -> chests.set("chests." + s, supplierChest.serialize()));
+    public void save(Location location) {
+        find(location).ifPresent(supplierChest -> chests.set("chests." + location, supplierChest.serialize()));
 
-        remove(s);
+        remove(location);
     }
 
     @Override
-    public void remove(String s) {
-        supplierChests.remove(s);
+    public void remove(Location location) {
+        SUPPLIER_CHESTS.remove(location);
     }
 
     @Override
-    public void add(String s, SupplierChest supplierChest) {
-        supplierChests.put(s, supplierChest);
+    public void add(Location location, SupplierChest supplierChest) {
+        SUPPLIER_CHESTS.put(location, supplierChest);
     }
 
     @Override
     public void saveAll() {
-        supplierChests.forEach((name, supplierChest) -> save(name));
+        SUPPLIER_CHESTS.forEach((name, supplierChest) -> save(name));
 
         chests.save();
     }
@@ -68,7 +66,7 @@ public class SupplierChestStorageManager implements Storage<SupplierChest, Strin
             return;
         }
 
-        chests.getConfigurationSection("chests").getKeys(false).forEach(name -> findFromData(name).ifPresent(supplierChest -> supplierChests.put(name, supplierChest)));
+        chests.getConfigurationSection("chests").getKeys(false).forEach(location -> findFromData(LocationSerializable.fromString(location)).ifPresent(supplierChest -> SUPPLIER_CHESTS.put(LocationSerializable.fromString(location), supplierChest)));
     }
 
 }
