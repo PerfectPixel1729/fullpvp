3c63e84707c066d703d8b642d9f440198cdccd00
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-26 08:48:07 -0500

    Binding new class, UserViewer

--- src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
+++ src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
@@ -5,6 +5,8 @@ import me.perfectpixel.fullpvp.chest.SupplierChest;
 import me.perfectpixel.fullpvp.chest.SupplierChestStorageManager;
 import me.perfectpixel.fullpvp.chest.creator.SupplierChestCreatorCache;
 import me.perfectpixel.fullpvp.chest.creator.UserCreator;
+import me.perfectpixel.fullpvp.chest.viewer.SupplierChestViewerCache;
+import me.perfectpixel.fullpvp.chest.viewer.UserViewer;
 import me.perfectpixel.fullpvp.user.User;
 import me.perfectpixel.fullpvp.user.UserStorageManager;
 
@@ -24,6 +26,7 @@ public class StorageModule extends AbstractModule {
         bind(Key.of(new TypeReference<Storage<User, UUID>>() {}, (Names.named("users")))).to(UserStorageManager.class).singleton();
         bind(Key.of(new TypeReference<Storage<UserCreator, UUID>>() {}, (Names.named("chests-creators")))).to(SupplierChestCreatorCache.class).singleton();
         bind(Key.of(new TypeReference<Storage<SupplierChest, Location>>() {}, (Names.named("chests")))).to(SupplierChestStorageManager.class).singleton();
+        bind(Key.of(new TypeReference<Storage<UserViewer, UUID>>() {}, (Names.named("chests-viewers")))).to(SupplierChestViewerCache.class).singleton();
     }
 
 }
