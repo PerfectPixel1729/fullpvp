bc54aff9b54a024914b84c27d782f70d16c48af0
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-26 08:45:25 -0500

    Removed unused attribute: Location.

--- src/main/java/me/perfectpixel/fullpvp/chest/DefaultSupplierChest.java
+++ src/main/java/me/perfectpixel/fullpvp/chest/DefaultSupplierChest.java
@@ -3,9 +3,8 @@ package me.perfectpixel.fullpvp.chest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import me.perfectpixel.fullpvp.utils.LocationSerializable;
+import me.perfectpixel.fullpvp.utils.ItemSerializable;
 
-import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
@@ -16,15 +15,14 @@ import java.util.Map;
 public class DefaultSupplierChest implements SupplierChest {
 
     private final Map<Integer, ItemStack> items;
+
     private final String name;
-    private final Location location;
 
     @Override
     public Map<String, Object> serialize() {
         Map<String, Object> supplierChestMap = new HashMap<>();
 
-        supplierChestMap.put("location", LocationSerializable.fromBukkit(location));
-        items.forEach((slot, item) -> supplierChestMap.put("items." + slot, item.serialize()));
+        items.forEach((slot, item) -> supplierChestMap.put(slot + "", ItemSerializable.encodeItem(item)));
 
         return supplierChestMap;
     }
