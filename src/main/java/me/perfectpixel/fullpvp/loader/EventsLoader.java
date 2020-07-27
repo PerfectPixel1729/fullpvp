61d76adf3ad78e5456819fc2a08d35b46520c719
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-27 09:33:41 -0500

    Just cleanup code!

--- src/main/java/me/perfectpixel/fullpvp/loader/EventsLoader.java
+++ src/main/java/me/perfectpixel/fullpvp/loader/EventsLoader.java
@@ -5,12 +5,9 @@ import me.perfectpixel.fullpvp.listeners.PlayerDeathListener;
 import me.perfectpixel.fullpvp.listeners.PlayerInteractListener;
 import me.perfectpixel.fullpvp.listeners.PlayerJoinListener;
 import me.perfectpixel.fullpvp.listeners.PlayerQuitListener;
-
 import me.yushust.inject.Inject;
-
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
-
 import team.unnamed.gui.listeners.MenuListeners;
 
 public class EventsLoader implements Loader {
@@ -18,10 +15,17 @@ public class EventsLoader implements Loader {
     @Inject
     private FullPVP fullPVP;
 
-    @Inject private PlayerJoinListener playerJoinListener;
-    @Inject private PlayerDeathListener playerDeathListener;
-    @Inject private PlayerQuitListener playerQuitListener;
-    @Inject private PlayerInteractListener playerInteractListener;
+    @Inject
+    private PlayerJoinListener playerJoinListener;
+
+    @Inject
+    private PlayerDeathListener playerDeathListener;
+
+    @Inject
+    private PlayerQuitListener playerQuitListener;
+
+    @Inject
+    private PlayerInteractListener playerInteractListener;
 
     @Override
     public void load() {
