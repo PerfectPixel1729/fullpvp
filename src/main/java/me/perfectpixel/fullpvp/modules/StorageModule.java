66cc65b136f2d71d3a1f7eb74691fb2c0d9d7bb3
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-27 09:36:36 -0500

    Added new binders to the new Cache/Storage!

--- src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
+++ src/main/java/me/perfectpixel/fullpvp/modules/StorageModule.java
@@ -1,19 +1,19 @@
 package me.perfectpixel.fullpvp.modules;
 
+import me.perfectpixel.fullpvp.Cache;
 import me.perfectpixel.fullpvp.Storage;
 import me.perfectpixel.fullpvp.chest.SupplierChest;
 import me.perfectpixel.fullpvp.chest.SupplierChestStorageManager;
 import me.perfectpixel.fullpvp.chest.creator.SupplierChestCreatorCache;
 import me.perfectpixel.fullpvp.chest.creator.UserCreator;
-import me.perfectpixel.fullpvp.chest.viewer.SupplierChestViewerCache;
+import me.perfectpixel.fullpvp.chest.editor.SupplierChestEditorCache;
+import me.perfectpixel.fullpvp.chest.viewer.SupplierChestViewerStorageManager;
 import me.perfectpixel.fullpvp.chest.viewer.UserViewer;
 import me.perfectpixel.fullpvp.user.User;
 import me.perfectpixel.fullpvp.user.UserStorageManager;
 
 import me.yushust.inject.bind.AbstractModule;
 import me.yushust.inject.identity.Key;
-import me.yushust.inject.identity.type.TypeReference;
-import me.yushust.inject.name.Names;
 
 import org.bukkit.Location;
 
@@ -23,10 +23,12 @@ public class StorageModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(Key.of(new TypeReference<Storage<User, UUID>>() {}, (Names.named("users")))).to(UserStorageManager.class).singleton();
-        bind(Key.of(new TypeReference<Storage<UserCreator, UUID>>() {}, (Names.named("chests-creators")))).to(SupplierChestCreatorCache.class).singleton();
-        bind(Key.of(new TypeReference<Storage<SupplierChest, Location>>() {}, (Names.named("chests")))).to(SupplierChestStorageManager.class).singleton();
-        bind(Key.of(new TypeReference<Storage<UserViewer, UUID>>() {}, (Names.named("chests-viewers")))).to(SupplierChestViewerCache.class).singleton();
+        bind(new Key<Storage<UUID, User>>() {}).to(UserStorageManager.class).singleton();
+        bind(new Key<Storage<Location, SupplierChest>>() {}).to(SupplierChestStorageManager.class).singleton();
+        bind(new Key<Storage<UUID, UserViewer>>() {}).to(SupplierChestViewerStorageManager.class).singleton();
+
+        bind(new Key<Cache<UUID, SupplierChest>>() {}).to(SupplierChestEditorCache.class).singleton();
+        bind(new Key<Cache<UUID, UserCreator>>() {}).to(SupplierChestCreatorCache.class).singleton();
     }
 
 }
