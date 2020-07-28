6cb2663956bb7adc713982fd452a567d1ece3826
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-28 15:26:02 -0500

    Starting with the commands, waiting for solve any ebcm error!

---
+++ src/main/java/me/perfectpixel/fullpvp/commands/ClanCommands.java
@@ -0,0 +1,11 @@
+package me.perfectpixel.fullpvp.commands;
+
+import me.fixeddev.ebcm.parametric.CommandClass;
+import me.fixeddev.ebcm.parametric.annotation.ACommand;
+
+@ACommand(names = {"clan", "clans", "clanes"})
+public class ClanCommands implements CommandClass {
+
+
+
+}
