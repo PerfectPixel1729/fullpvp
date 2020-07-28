7bf7bc04d40306a981732a3cbc769f49363ef405
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-27 21:58:26 -0500

    Change FileManager name to FileCreator!

--- src/main/java/me/perfectpixel/fullpvp/modules/MainModule.java
+++ src/main/java/me/perfectpixel/fullpvp/modules/MainModule.java
@@ -2,7 +2,7 @@ package me.perfectpixel.fullpvp.modules;
 
 import me.perfectpixel.fullpvp.FullPVP;
 import me.perfectpixel.fullpvp.files.FileBinder;
-import me.perfectpixel.fullpvp.files.FileManager;
+import me.perfectpixel.fullpvp.files.FileCreator;
 
 import me.yushust.inject.bind.AbstractModule;
 
@@ -18,11 +18,11 @@ public class MainModule extends AbstractModule {
     protected void configure() {
 
         FileBinder fileBinder = new FileBinder()
-                .bind("data", new FileManager(fullPVP, "data"))
-                .bind("config", new FileManager(fullPVP, "config"))
-                .bind("language", new FileManager(fullPVP, "language"))
-                .bind("chests", new FileManager(fullPVP, "chests"))
-                .bind("menu", new FileManager(fullPVP, "menu"));
+                .bind("data", new FileCreator(fullPVP, "data"))
+                .bind("config", new FileCreator(fullPVP, "config"))
+                .bind("language", new FileCreator(fullPVP, "language"))
+                .bind("chests", new FileCreator(fullPVP, "chests"))
+                .bind("menu", new FileCreator(fullPVP, "menu"));
 
         install(fileBinder.build());
         install(new MessageModule());
