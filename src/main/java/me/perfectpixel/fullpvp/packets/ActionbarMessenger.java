12e2932ff7a1ed52164806d6dfc30095fbf7bfaf
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-27 21:55:58 -0500

    Added simple packet: Actionbar, to enderpearl's countdown's

---
+++ src/main/java/me/perfectpixel/fullpvp/packets/ActionbarMessenger.java
@@ -0,0 +1,21 @@
+package me.perfectpixel.fullpvp.packets;
+
+import me.yushust.inject.process.annotation.Singleton;
+
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+@Singleton
+public class ActionbarMessenger {
+
+    public void sendActionbar(Player player, String message) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        IChatBaseComponent chatBaseComponent = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
+        PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(chatBaseComponent, (byte) 2);
+        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutChat);
+    }
+
+}
