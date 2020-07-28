fc906547b650151f77b2a4b405fedfd1a599ac8c
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-28 18:07:47 -0500

    Just cleanup code!

--- src/main/java/me/perfectpixel/fullpvp/message/SimpleMessageDecorator.java
+++ src/main/java/me/perfectpixel/fullpvp/message/SimpleMessageDecorator.java
@@ -2,7 +2,9 @@ package me.perfectpixel.fullpvp.message;
 
 import me.perfectpixel.fullpvp.Delegates;
 import me.perfectpixel.fullpvp.message.placeholder.PlaceholderApplier;
+
 import me.yushust.inject.Inject;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -10,8 +12,12 @@ import java.util.List;
 
 public class SimpleMessageDecorator implements Message {
 
-    @Inject @Delegates private Message delegate;
-    @Inject private PlaceholderApplier placeholderApplier;
+    @Inject
+    @Delegates
+    private Message delegate;
+
+    @Inject
+    private PlaceholderApplier placeholderApplier;
 
     @Override
     public String getMessage(Player player, String id) {
@@ -22,6 +28,7 @@ public class SimpleMessageDecorator implements Message {
     public List<String> getMessages(Player player, String messageId) {
         List<String> messages = delegate.getMessages(player, messageId);
         messages.replaceAll(line -> format(player, line));
+
         return messages;
     }
 
