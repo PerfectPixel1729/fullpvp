1029775cce46de61937c2bd2654efce04e8d63a6
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-28 18:07:39 -0500

    Changed FileCreator!

--- src/main/java/me/perfectpixel/fullpvp/commands/SimpleI18n.java
+++ src/main/java/me/perfectpixel/fullpvp/commands/SimpleI18n.java
@@ -4,10 +4,9 @@ import me.fixeddev.ebcm.NamespaceAccesor;
 import me.fixeddev.ebcm.i18n.I18n;
 import me.fixeddev.ebcm.i18n.Message;
 
-import me.perfectpixel.fullpvp.files.FileCreator;
+import me.perfectpixel.fullpvp.Delegates;
 
 import me.yushust.inject.Inject;
-import me.yushust.inject.name.Named;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,13 +16,13 @@ public class SimpleI18n implements I18n {
     private final Map<String, String> messageMap = new HashMap<>();
 
     @Inject
-    public SimpleI18n(@Named("language") FileCreator language) {
-        messageMap.put(Message.COMMAND_NO_PERMISSIONS.getId(), language.getString("i18n.no-permission"));
+    public SimpleI18n(@Delegates me.perfectpixel.fullpvp.message.Message message) {
+        messageMap.put(Message.COMMAND_NO_PERMISSIONS.getId(), message.getMessage(null, "i18n.no-permission"));
         messageMap.put(Message.COMMAND_USAGE.getId(), null);
-        messageMap.put(Message.INVALID_SUBCOMMAND.getId(), language.getString("i18n.invalid-subcommand"));
-        messageMap.put(Message.MISSING_ARGUMENT.getId(), language.getString("i18n.missing-argument"));
-        messageMap.put(Message.MISSING_SUBCOMMAND.getId(), language.getString("i18n.missing-subcommand"));
-        messageMap.put("provider.invalid.int", language.getString("i18n.invalid-int"));
+        messageMap.put(Message.INVALID_SUBCOMMAND.getId(), message.getMessage(null,"i18n.invalid-subcommand"));
+        messageMap.put(Message.MISSING_ARGUMENT.getId(), message.getMessage(null,"i18n.missing-argument"));
+        messageMap.put(Message.MISSING_SUBCOMMAND.getId(), message.getMessage(null,"i18n.missing-subcommand"));
+        messageMap.put("provider.invalid.int", message.getMessage(null,"i18n.invalid-int"));
     }
 
     @Override
