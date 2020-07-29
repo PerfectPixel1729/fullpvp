02a3c137b2230bbf0965ad1ed414c71fe07038cc
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-29 12:02:44 -0500

    Added a method to check if a player has clan!

--- src/main/java/me/perfectpixel/fullpvp/clans/ClanUtilities.java
+++ src/main/java/me/perfectpixel/fullpvp/clans/ClanUtilities.java
@@ -1,6 +1,7 @@
 package me.perfectpixel.fullpvp.clans;
 
 import me.perfectpixel.fullpvp.Storage;
+import me.perfectpixel.fullpvp.user.User;
 
 import me.yushust.inject.Inject;
 import me.yushust.inject.process.annotation.Singleton;
@@ -9,12 +10,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.UUID;
 
 @Singleton
 public class ClanUtilities {
 
     @Inject
     private Storage<String, Clan> clanStorage;
+
+    @Inject
+    private Storage<UUID, User> userStorage;
 
     public void sendMessageToMembers(String clanName, String message) {
         clanStorage.find(clanName).ifPresent(clan -> clan.getMembers().forEach(uuid -> {
@@ -30,6 +35,10 @@ public class ClanUtilities {
 
             message.forEach(member::sendMessage);
         }));
+    }
+
+    public boolean playerHasClan(Player player) {
+        return userStorage.find(player.getUniqueId()).map(user -> user.getClanName().isPresent()).orElse(false);
     }
 
 }
