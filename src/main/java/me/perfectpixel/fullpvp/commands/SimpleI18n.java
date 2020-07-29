4ae5bb2f072f1ff6b2595860276f08dc4b671d91
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-29 00:08:10 -0500

    Remove duplicate code, now inheritance of DefaultI18n

--- src/main/java/me/perfectpixel/fullpvp/commands/SimpleI18n.java
+++ src/main/java/me/perfectpixel/fullpvp/commands/SimpleI18n.java
@@ -1,7 +1,6 @@
 package me.perfectpixel.fullpvp.commands;
 
-import me.fixeddev.ebcm.NamespaceAccesor;
-import me.fixeddev.ebcm.i18n.I18n;
+import me.fixeddev.ebcm.i18n.DefaultI18n;
 import me.fixeddev.ebcm.i18n.Message;
 
 import me.perfectpixel.fullpvp.Delegates;
@@ -11,37 +10,18 @@ import me.yushust.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SimpleI18n implements I18n {
+public class SimpleI18n extends DefaultI18n {
 
     private final Map<String, String> messageMap = new HashMap<>();
 
     @Inject
     public SimpleI18n(@Delegates me.perfectpixel.fullpvp.message.Message message) {
-        messageMap.put(Message.COMMAND_NO_PERMISSIONS.getId(), message.getMessage(null, "i18n.no-permission"));
-        messageMap.put(Message.COMMAND_USAGE.getId(), null);
-        messageMap.put(Message.INVALID_SUBCOMMAND.getId(), message.getMessage(null,"i18n.invalid-subcommand"));
-        messageMap.put(Message.MISSING_ARGUMENT.getId(), message.getMessage(null,"i18n.missing-argument"));
-        messageMap.put(Message.MISSING_SUBCOMMAND.getId(), message.getMessage(null,"i18n.missing-subcommand"));
-        messageMap.put("provider.invalid.int", message.getMessage(null,"i18n.invalid-int"));
-    }
-
-    @Override
-    public String getMessage(String messageId, NamespaceAccesor namespaceAccesor) {
-        String message = messageMap.get(messageId);
-
-        if (message != null) {
-            return message;
-        }
-
-        Message messageObject = Message.findMessage(messageId);
-
-        if (messageObject == null) {
-            return null;
-        }
-
-        messageId = messageObject.getId();
-
-        return messageMap.get(messageId);
+        setMessage(Message.COMMAND_NO_PERMISSIONS.getId(), message.getMessage(null, "i18n.no-permission"));
+        setMessage(Message.COMMAND_USAGE.getId(), message.getMessage(null, "i18n.invalid-usage"));
+        setMessage(Message.INVALID_SUBCOMMAND.getId(), message.getMessage(null,"i18n.invalid-subcommand"));
+        setMessage(Message.MISSING_ARGUMENT.getId(), message.getMessage(null,"i18n.missing-argument"));
+        setMessage(Message.MISSING_SUBCOMMAND.getId(), message.getMessage(null,"i18n.missing-subcommand"));
+        setMessage("provider.invalid.int", message.getMessage(null,"i18n.invalid-int"));
     }
 
 }
