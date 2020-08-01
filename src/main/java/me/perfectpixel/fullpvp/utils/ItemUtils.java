75c60dee78591ae061883374427d35091b6af775
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-01 10:52:47 -0500

    Simple class to item utilities!

---
+++ src/main/java/me/perfectpixel/fullpvp/utils/ItemUtils.java
@@ -0,0 +1,25 @@
+package me.perfectpixel.fullpvp.utils;
+
+import me.yushust.inject.process.annotation.Singleton;
+
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+
+import org.bukkit.inventory.ItemStack;
+
+@Singleton
+public class ItemUtils {
+
+    public ItemStack addNBTTag(ItemStack input, String key, String value) {
+        net.minecraft.server.v1_8_R3.ItemStack itemNMS = CraftItemStack.asNMSCopy(input);
+
+        NBTTagCompound tagCompound = itemNMS.hasTag() ? itemNMS.getTag() : new NBTTagCompound();
+
+        tagCompound.setString(key, value);
+
+        itemNMS.setTag(tagCompound);
+
+        return CraftItemStack.asBukkitCopy(itemNMS);
+    }
+
+}
