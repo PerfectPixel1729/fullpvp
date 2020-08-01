7ab1d24c1f1900e0da43cb2e4e07be770adc6e57
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-01 10:54:13 -0500

    Added a Player parameter!

--- src/main/java/me/perfectpixel/fullpvp/menus/Menu.java
+++ src/main/java/me/perfectpixel/fullpvp/menus/Menu.java
@@ -1,9 +1,10 @@
 package me.perfectpixel.fullpvp.menus;
 
+import org.bukkit.entity.Player;
 import team.unnamed.gui.menu.MenuBuilder;
 
 public interface Menu {
 
-    MenuBuilder build();
+    MenuBuilder build(Player player);
 
 }
