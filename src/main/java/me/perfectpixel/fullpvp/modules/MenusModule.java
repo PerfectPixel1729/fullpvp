44e0ccee2e7376f82da2d6e3e84493bb22ca4400
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-01 10:55:00 -0500

    Binding two new menus of clan system!

--- src/main/java/me/perfectpixel/fullpvp/modules/MenusModule.java
+++ src/main/java/me/perfectpixel/fullpvp/modules/MenusModule.java
@@ -1,6 +1,8 @@
 package me.perfectpixel.fullpvp.modules;
 
 import me.perfectpixel.fullpvp.menus.ChestCreatorMenu;
+import me.perfectpixel.fullpvp.menus.clans.ClanDisbandMenu;
+import me.perfectpixel.fullpvp.menus.clans.ClanMainMenu;
 import me.perfectpixel.fullpvp.menus.Menu;
 
 import me.yushust.inject.bind.AbstractModule;
@@ -10,6 +12,8 @@ public class MenusModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Menu.class).named("chest-creator").to(ChestCreatorMenu.class).singleton();
+        bind(Menu.class).named("clan-main").to(ClanMainMenu.class).singleton();
+        bind(Menu.class).named("clan-disband").to(ClanDisbandMenu.class).singleton();
     }
 
 }
