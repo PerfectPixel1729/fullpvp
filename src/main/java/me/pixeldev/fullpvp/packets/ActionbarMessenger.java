d53bcf2ed23f556ed22bd6566b550501b58ebb08
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-05 15:00:50 -0500

    Cleanup and simplify code!

--- src/main/java/me/pixeldev/fullpvp/packets/ActionbarMessenger.java
+++ src/main/java/me/pixeldev/fullpvp/packets/ActionbarMessenger.java
@@ -1,21 +1,23 @@
 package me.pixeldev.fullpvp.packets;
 
+import team.unnamed.inject.Inject;
 import team.unnamed.inject.process.annotation.Singleton;
 
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 @Singleton
 public class ActionbarMessenger {
 
+    @Inject
+    private NMSUtils nmsUtils;
+
     public void sendActionbar(Player player, String message) {
-        CraftPlayer craftPlayer = (CraftPlayer) player;
         IChatBaseComponent chatBaseComponent = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
         PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(chatBaseComponent, (byte) 2);
-        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutChat);
+        nmsUtils.sendPacket(player, packetPlayOutChat);
     }
 
 }
