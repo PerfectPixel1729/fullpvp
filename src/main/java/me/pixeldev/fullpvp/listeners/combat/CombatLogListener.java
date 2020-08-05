9fda82334c451344d307e90e7e58b7ed9d5ae916
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-05 12:21:37 -0500

    Finished all listeners to combat log system!

--- src/main/java/me/pixeldev/fullpvp/listeners/combat/CombatLogListener.java
+++ src/main/java/me/pixeldev/fullpvp/listeners/combat/CombatLogListener.java
@@ -1,18 +1,29 @@
 package me.pixeldev.fullpvp.listeners.combat;
 
 import me.pixeldev.fullpvp.Cache;
-import me.pixeldev.fullpvp.combatlog.CombatLogAnnouncer;
+import me.pixeldev.fullpvp.combatlog.announcer.CombatLogAnnouncer;
 import me.pixeldev.fullpvp.event.FullPVPTickEvent;
+import me.pixeldev.fullpvp.files.FileCreator;
+import me.pixeldev.fullpvp.message.Message;
+import me.pixeldev.fullpvp.utils.InventoryUtils;
 import me.pixeldev.fullpvp.utils.TickCause;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 import team.unnamed.inject.Inject;
 import team.unnamed.inject.name.Named;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CombatLogListener implements Listener {
@@ -23,6 +34,16 @@ public class CombatLogListener implements Listener {
 
     @Inject
     private CombatLogAnnouncer combatLogAnnouncer;
+
+    @Inject
+    @Named("config")
+    private FileCreator config;
+
+    @Inject
+    private Message message;
+
+    @Inject
+    private InventoryUtils inventoryUtils;
 
     @EventHandler
     public void onServerTick(FullPVPTickEvent event) {
@@ -52,6 +73,95 @@ public class CombatLogListener implements Listener {
         }
 
         combatLogCache.get().keySet().forEach(uuid -> combatLogAnnouncer.sendPlayerLog(Bukkit.getPlayer(uuid)));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        if (combatLogCache.exists(player.getUniqueId())) {
+            combatLogCache.remove(player.getUniqueId());
+
+            combatLogAnnouncer.sendPlayerDisconnect(player);
+
+            player.setHealth(0);
+        }
+    }
+
+    @EventHandler
+    public void onPreProcessCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+
+        if (combatLogCache.exists(player.getUniqueId())) {
+            String command = event.getMessage().split("/")[1];
+
+            List<String> blockedCommands = config.getStringList("combatlog.block-commands");
+
+            if (blockedCommands.contains(command.toLowerCase())) {
+                event.setCancelled(true);
+
+                player.sendMessage(message.getMessage(player, "combatlog.can-not-execute-command"));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
+        Entity entitySender = event.getDamager();
+        Entity entityTarget = event.getEntity();
+
+        if (!(entityTarget instanceof Player)) {
+            return;
+        }
+
+        int duration = config.getInt("combatlog.duration");
+
+        Player playerSender = null;
+        Player playerTarget = (Player) entityTarget;
+
+        if (entitySender instanceof Projectile) {
+            Projectile projectile = (Projectile) entitySender;
+
+            ProjectileSource shooter = projectile.getShooter();
+
+            if (shooter instanceof Player) {
+                playerSender = (Player) shooter;
+            }
+        } else if (entitySender instanceof Player) {
+            playerSender = (Player) entitySender;
+        }
+
+        if (playerSender != null) {
+            UUID uuidSender = playerSender.getUniqueId();
+            UUID uuidTarget = playerTarget.getUniqueId();
+
+            if (uuidSender.equals(uuidTarget)) {
+                return;
+            }
+
+            combatLogCache.add(uuidSender, duration);
+            combatLogAnnouncer.sendPlayerLog(playerSender);
+
+            disableFly(playerSender);
+            disableFly(playerTarget);
+
+            combatLogCache.add(uuidTarget, duration);
+            combatLogAnnouncer.sendPlayerLog(playerTarget);
+        }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+
+        if (combatLogCache.exists(player.getUniqueId())) {
+            event.setCancelled(true);
+        }
+    }
+
+    private void disableFly(Player player) {
+        player.setAllowFlight(false);
+        player.setFlying(false);
     }
 
 }
