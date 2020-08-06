65c1bf1fdacb18c069c1d8923c3b28d8b86ae002
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-06 16:16:29 -0500

    Idk, just change to class.

--- src/main/java/me/pixeldev/fullpvp/utils/LocationSerializable.java
+++ src/main/java/me/pixeldev/fullpvp/utils/LocationSerializable.java
@@ -3,15 +3,15 @@ package me.pixeldev.fullpvp.utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
-public interface LocationSerializable {
+public class LocationSerializable {
 
-    static Location fromString(String location) {
+    public static Location fromString(String location) {
         String[] split = location.split(";");
 
         return new Location(Bukkit.getWorld(split[0]), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
     }
 
-    static String fromBukkit(Location location) {
+    public static String fromBukkit(Location location) {
         return location.getWorld().getName() + ";" + location.getX() + ";" + location.getY() + ";" + location.getZ();
     }
 
