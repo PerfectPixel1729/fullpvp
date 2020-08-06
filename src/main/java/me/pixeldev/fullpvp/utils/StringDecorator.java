7ba9ce0de10db36d7a05057018e8f6bdae486e78
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-06 15:50:51 -0500

    Added a string decorator to center strings.

---
+++ src/main/java/me/pixeldev/fullpvp/utils/StringDecorator.java
@@ -0,0 +1,45 @@
+package me.pixeldev.fullpvp.utils;
+
+import team.unnamed.inject.process.annotation.Singleton;
+
+@Singleton
+public class StringDecorator {
+
+    public String getCenteredMessage(String message) {
+        if (message == null || message.equals("")) {
+            return "";
+        }
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for (char c : message.toCharArray()) {
+            if (c == '§') {
+                previousCode = true;
+            } else if (previousCode) {
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            } else {
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = 154 - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (compensated < toCompensate) {
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+
+        return sb.toString() + message;
+    }
+
+}
