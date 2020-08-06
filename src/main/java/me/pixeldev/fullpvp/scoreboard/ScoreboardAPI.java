5a2c3a05a8d6e925795500776e5253bf9edf4f6b
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-06 18:53:14 -0500

    Remove scoreboard classes!

--- src/main/java/me/pixeldev/fullpvp/scoreboard/ScoreboardAPI.java
+++
@@ -1,4 +0,0 @@
-package me.pixeldev.fullpvp.scoreboard;
-
-public class ScoreboardAPI {
-}
--- src/main/java/me/pixeldev/fullpvp/scoreboard/animation/ScoreAnimation.java
+++
@@ -1,7 +0,0 @@
-package me.pixeldev.fullpvp.scoreboard.animation;
-
-public enum  ScoreAnimation {
-
-    SHINE
-
-}
