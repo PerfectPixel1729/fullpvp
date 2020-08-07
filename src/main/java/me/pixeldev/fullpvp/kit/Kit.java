f1fdf913fcd1107fc90c68358e8351567f96c86d
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-06 22:16:57 -0500

    Added methods to serialize kit!

--- src/main/java/me/pixeldev/fullpvp/kit/Kit.java
+++ src/main/java/me/pixeldev/fullpvp/kit/Kit.java
@@ -1,14 +1,37 @@
 package me.pixeldev.fullpvp.kit;
 
+import me.pixeldev.fullpvp.utils.ItemSerializable;
+
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 public interface Kit {
 
-    Optional<List<ItemStack>> getArmorContents();
+    List<ItemStack> getArmorContents();
 
     List<ItemStack> getContents();
+
+    default Map<String, Object> serializeArmor() {
+        Map<String, Object> armorMap = new HashMap<>();
+
+        for (int i = 0; i < getArmorContents().size(); i++) {
+            armorMap.put(i + "", ItemSerializable.encodeItem(getArmorContents().get(i)));
+        }
+
+        return armorMap;
+    }
+
+    default Map<String, Object> serializeContents() {
+        Map<String, Object> contentsMap = new HashMap<>();
+
+        for (int i = 0; i < getContents().size(); i++) {
+            contentsMap.put(i + "", ItemSerializable.encodeItem(getContents().get(i)));
+        }
+
+        return contentsMap;
+    }
 
 }
